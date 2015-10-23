25baafe7df1a66d1e28c0a3978240557494873f8
andreadelrio <delrio.andre@gmail.com>
2015-10-23 00:06:28 -0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation();
+
+ $("#fpi-grid li").click(function(){
+ 	// $('[data-pri-details=' + $(this).data("pri-num") + ']').siblings().addClass("disno");	
+ 	$('.details-box').addClass('disno');
+ 	$('[data-fpi-details=' + $(this).data("fpi-num") + ']').toggleClass("disno");	
+ });
